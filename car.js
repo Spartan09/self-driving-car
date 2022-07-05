8bbfb48dca9d1daf0531dd90b4bc277b91ae92b9
Ayushman Singh Rathore <ayushsg0412@gmail.com>
2022-07-05 14:38:56 +0530

    add collision detection

--- car.js
+++ car.js
@@ -1,58 +1,37 @@
-class Car {
-    constructor(x, y, width, height) {
-        this.x = x
-        this.y = y
-        this.width = width
-        this.height = height
+class Car{
+    constructor(x,y,width,height){
+        this.x=x;
+        this.y=y;
+        this.width=width;
+        this.height=height;
 
-        this.speed = 0
-        this.acceleration = 0.2
-        this.maxSpeed = 3
-        this.friction = 0.05
-        this.angle = 0
+        this.speed=0;
+        this.acceleration=0.2;
+        this.maxSpeed=3;
+        this.friction=0.05;
+        this.angle=0;
+        this.damaged=false;
 
-        this.sensor = new Sensor(this)
+        this.sensor=new Sensor(this);
         this.controls=new Controls();
     }
 
-    #move() {
-        if (this.controls.forward) {
-            this.speed += this.acceleration
+    update(roadBorders){
+        if(!this.damaged){
+            this.#move();
+            this.polygon=this.#createPolygon();
+            this.damaged=this.#assessDamage(roadBorders);
         }
-        if (this.controls.reverse) {
-            this.speed -= this.acceleration
-        }
+        this.sensor.update(roadBorders);
+    }
 
-        if (this.speed > this.maxSpeed) {
-            this.speed = this.maxSpeed
-        }
-        if (this.speed < -this.maxSpeed/2) {
-            this.speed = -this.maxSpeed/2
-        }
-
-        if (this.speed > 0) {
-            this.speed -= this.friction
-        }
-        if (this.speed < 0) {
-            this.speed += this.friction
-        }
-
-        if (Math.abs(this.speed) < this.friction) {
-            this.speed = 0
-        }
-
-        if (this.speed !== 0) {
-            const flip = this.speed > 0 ? 1 : -1
-            if (this.controls.left) {
-                this.angle += 0.03 * flip
-            }
-            if (this.controls.right) {
-                this.angle -= 0.03 * flip
+    #assessDamage(roadBorders){
+        for(let i=0;i<roadBorders.length;i++){
+            if(polysIntersect(this.polygon,roadBorders[i])){
+                return true;
             }
         }
-
-        this.x -= Math.sin(this.angle)*this.speed
-        this.y -= Math.cos(this.angle)*this.speed
+        return false;
     }
 
     #createPolygon(){
@@ -78,21 +57,58 @@ class Car {
         return points;
     }
 
-    update(roadBorders) {
-        this.#move()
-        this.polygon = this.#createPolygon()
-        this.sensor.update(roadBorders)
+    #move(){
+        if(this.controls.forward){
+            this.speed+=this.acceleration;
+        }
+        if(this.controls.reverse){
+            this.speed-=this.acceleration;
+        }
+
+        if(this.speed>this.maxSpeed){
+            this.speed=this.maxSpeed;
+        }
+        if(this.speed<-this.maxSpeed/2){
+            this.speed=-this.maxSpeed/2;
+        }
+
+        if(this.speed>0){
+            this.speed-=this.friction;
+        }
+        if(this.speed<0){
+            this.speed+=this.friction;
+        }
+        if(Math.abs(this.speed)<this.friction){
+            this.speed=0;
+        }
+
+        if(this.speed!=0){
+            const flip=this.speed>0?1:-1;
+            if(this.controls.left){
+                this.angle+=0.03*flip;
+            }
+            if(this.controls.right){
+                this.angle-=0.03*flip;
+            }
+        }
+
+        this.x-=Math.sin(this.angle)*this.speed;
+        this.y-=Math.cos(this.angle)*this.speed;
     }
 
-
-
-    draw(ctx) {
-        ctx.beginPath()
-        ctx.moveTo(this.polygon[0].x, this.polygon[0].y)
-        for (let i=1; i < this.polygon.length; i++) {
-            ctx.lineTo(this.polygon[i].x, this.polygon[i].y)
+    draw(ctx){
+        if(this.damaged){
+            ctx.fillStyle="gray";
+        }else{
+            ctx.fillStyle="black";
         }
-        ctx.fill()
-        this.sensor.draw(ctx)
+        ctx.beginPath();
+        ctx.moveTo(this.polygon[0].x,this.polygon[0].y);
+        for(let i=1;i<this.polygon.length;i++){
+            ctx.lineTo(this.polygon[i].x,this.polygon[i].y);
+        }
+        ctx.fill();
+
+        this.sensor.draw(ctx);
     }
 }
--- sensor.js
+++ sensor.js
@@ -1,9 +1,9 @@
 class Sensor {
     constructor(car) {
         this.car = car;
-        this.rayCount = 15;
+        this.rayCount = 10;
         this.rayLength = 150;
-        this.raySpread = Math.PI / 2;
+        this.raySpread = Math.PI/2 ;
 
         this.rays = [];
         this.readings = [];
