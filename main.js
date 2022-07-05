8e0e30f3669228416209c2805feb6efed110081a
Ayushman Singh Rathore <ayushsg0412@gmail.com>
2022-07-05 11:09:07 +0530

    road complete

--- main.js
+++ main.js
@@ -2,14 +2,20 @@ const canvas = document.getElementById('myCanvas');
 canvas.width = 200;
 
 const ctx = canvas.getContext('2d')
-const road = new Road(canvas.width/2, canvas.width)
-const car = new Car(100, 100, 30, 50)
+const road = new Road(canvas.width/2, canvas.width * 0.9)
+const car = new Car(road.getLaneCenter(1), 100, 30, 50)
 
 function animate() {
     car.update()
     canvas.height = window.innerHeight;
+
+    ctx.save()
+    ctx.translate(0, -car.y + canvas.height * 0.75)
+
     road.draw(ctx)
     car.draw(ctx)
+
+    ctx.restore()
     window.requestAnimationFrame(animate)
 }
 
