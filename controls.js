30bd04728cdb9e2f3bf62e7e758761097b9f3586
Ayushman Singh Rathore <ayushsg0412@gmail.com>
2022-07-05 14:54:48 +0530

    add traffic simulation

--- controls.js
+++ controls.js
@@ -1,45 +1,52 @@
 class Controls {
-    constructor() {
-        this.forward = false
-        this.left = false
-        this.right = false
-        this.reverse = false
+    constructor(type) {
+        this.forward = false;
+        this.left = false;
+        this.right = false;
+        this.reverse = false;
 
-        this.#addKeyboardListeners()
+        switch (type) {
+            case "KEYS":
+                this.#addKeyboardListeners();
+                break;
+            case "DUMMY":
+                this.forward = true;
+                break;
+        }
     }
 
     #addKeyboardListeners() {
-        document.onkeydown = event => {
-            switch(event.key) {
+        document.onkeydown = (event) => {
+            switch (event.key) {
                 case "ArrowLeft":
-                    this.left = true
-                    break
-                case "ArrowRight":
-                    this.right = true
-                    break
-                case "ArrowUp":
-                    this.forward = true
-                    break
-                case "ArrowDown":
-                    this.reverse = true
-                    break
-            }
-        }
-        document.onkeyup=(event)=>{
-            switch(event.key){
-                case "ArrowLeft":
-                    this.left=false;
+                    this.left = true;
                     break;
                 case "ArrowRight":
-                    this.right=false;
+                    this.right = true;
                     break;
                 case "ArrowUp":
-                    this.forward=false;
+                    this.forward = true;
                     break;
                 case "ArrowDown":
-                    this.reverse=false;
+                    this.reverse = true;
                     break;
             }
-        }
+        };
+        document.onkeyup = (event) => {
+            switch (event.key) {
+                case "ArrowLeft":
+                    this.left = false;
+                    break;
+                case "ArrowRight":
+                    this.right = false;
+                    break;
+                case "ArrowUp":
+                    this.forward = false;
+                    break;
+                case "ArrowDown":
+                    this.reverse = false;
+                    break;
+            }
+        };
     }
 }
--- sensor.js
+++ sensor.js
@@ -1,12 +1,61 @@
 class Sensor {
     constructor(car) {
         this.car = car;
-        this.rayCount = 10;
+        this.rayCount = 5;
         this.rayLength = 150;
-        this.raySpread = Math.PI/2 ;
+        this.raySpread = Math.PI / 2;
 
         this.rays = [];
         this.readings = [];
+    }
+
+    update(roadBorders, traffic) {
+        this.#castRays();
+        this.readings = [];
+        for (let i = 0; i < this.rays.length; i++) {
+            this.readings.push(
+                this.#getReading(this.rays[i], roadBorders, traffic)
+            );
+        }
+    }
+
+    #getReading(ray, roadBorders, traffic) {
+        let touches = [];
+
+        for (let i = 0; i < roadBorders.length; i++) {
+            const touch = getIntersection(
+                ray[0],
+                ray[1],
+                roadBorders[i][0],
+                roadBorders[i][1]
+            );
+            if (touch) {
+                touches.push(touch);
+            }
+        }
+
+        for (let i = 0; i < traffic.length; i++) {
+            const poly = traffic[i].polygon;
+            for (let j = 0; j < poly.length; j++) {
+                const value = getIntersection(
+                    ray[0],
+                    ray[1],
+                    poly[j],
+                    poly[(j + 1) % poly.length]
+                );
+                if (value) {
+                    touches.push(value);
+                }
+            }
+        }
+
+        if (touches.length == 0) {
+            return null;
+        } else {
+            const offsets = touches.map((e) => e.offset);
+            const minOffset = Math.min(...offsets);
+            return touches.find((e) => e.offset == minOffset);
+        }
     }
 
     #castRays() {
@@ -25,38 +74,6 @@ class Sensor {
                 y: this.car.y - Math.cos(rayAngle) * this.rayLength,
             };
             this.rays.push([start, end]);
-        }
-    }
-
-    #getReading(ray, roadBorders) {
-        let touches = [];
-
-        for (let i = 0; i < roadBorders.length; i++) {
-            const touch = getIntersection(
-                ray[0],
-                ray[1],
-                roadBorders[i][0],
-                roadBorders[i][1]
-            );
-            if (touch) {
-                touches.push(touch);
-            }
-        }
-
-        if (touches.length == 0) {
-            return null;
-        } else {
-            const offsets = touches.map((e) => e.offset);
-            const minOffset = Math.min(...offsets);
-            return touches.find((e) => e.offset == minOffset);
-        }
-    }
-
-    update(roadBorders) {
-        this.#castRays();
-        this.readings = [];
-        for (let i = 0; i < this.rays.length; i++) {
-            this.readings.push(this.#getReading(this.rays[i], roadBorders));
         }
     }
 
