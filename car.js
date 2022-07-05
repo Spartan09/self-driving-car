f384e56f77e9585d6b37c44ee726c21a08bb00f1
Ayushman Singh Rathore <ayushsg0412@gmail.com>
2022-07-05 13:46:53 +0530

    add sensor to car

--- car.js
+++ car.js
@@ -11,6 +11,7 @@ class Car {
         this.friction = 0.05
         this.angle = 0
 
+        this.sensor = new Sensor(this)
         this.controls=new Controls();
     }
 
@@ -54,25 +55,44 @@ class Car {
         this.y -= Math.cos(this.angle)*this.speed
     }
 
-    update() {
+    #createPolygon(){
+        const points=[];
+        const rad=Math.hypot(this.width,this.height)/2;
+        const alpha=Math.atan2(this.width,this.height);
+        points.push({
+            x:this.x-Math.sin(this.angle-alpha)*rad,
+            y:this.y-Math.cos(this.angle-alpha)*rad
+        });
+        points.push({
+            x:this.x-Math.sin(this.angle+alpha)*rad,
+            y:this.y-Math.cos(this.angle+alpha)*rad
+        });
+        points.push({
+            x:this.x-Math.sin(Math.PI+this.angle-alpha)*rad,
+            y:this.y-Math.cos(Math.PI+this.angle-alpha)*rad
+        });
+        points.push({
+            x:this.x-Math.sin(Math.PI+this.angle+alpha)*rad,
+            y:this.y-Math.cos(Math.PI+this.angle+alpha)*rad
+        });
+        return points;
+    }
+
+    update(roadBorders) {
         this.#move()
+        this.polygon = this.#createPolygon()
+        this.sensor.update(roadBorders)
     }
 
 
 
     draw(ctx) {
-        ctx.save()
-        ctx.translate(this.x, this.y)
-        ctx.rotate(-this.angle)
-
         ctx.beginPath()
-        ctx.rect(
-            -this.width/2,
-            -this.height/2,
-            this.width,
-            this.height
-        )
+        ctx.moveTo(this.polygon[0].x, this.polygon[0].y)
+        for (let i=1; i < this.polygon.length; i++) {
+            ctx.lineTo(this.polygon[i].x, this.polygon[i].y)
+        }
         ctx.fill()
-        ctx.restore()
+        this.sensor.draw(ctx)
     }
 }
--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const road = new Road(canvas.width/2, canvas.width * 0.9)
 const car = new Car(road.getLaneCenter(1), 100, 30, 50)
 
 function animate() {
-    car.update()
+    car.update(road.borders)
     canvas.height = window.innerHeight;
 
     ctx.save()
